b8a355c4bea002b0d8bd58342c7ebd5d8812116a
masa2 <masa00222266@gmail.com>
2013-12-05 14:46:49 +0900

    ボタンを押すと、animationスタート

--- src/com/example/android_animation/MainActivity.java
+++ src/com/example/android_animation/MainActivity.java
@@ -3,9 +3,14 @@
 //http://www.geocities.jp/inu_poti/java/meida/image02.html
 package com.example.android_animation;
 
+//import com.example.musicgame4.R;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 //import android.view.animation.Animation;
@@ -18,41 +23,89 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity  {
+public class MainActivity extends Activity implements OnTouchListener {
+	
+	int testbtn1, testbtn2, testbtn3, testbtn4;
+	ImageView img1, img2;		
+	ImageButton imgbtn1, imgbtn2;
+	Button btn1, btn2, btn3, btn4;
+	Animation anim1, anim2;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		ImageView img1 = (ImageView)findViewById(R.id.MusicIcon1);
-		ImageView img2 = (ImageView)findViewById(R.id.MusicIcon2);
+		img1 = (ImageView)findViewById(R.id.MusicIcon1);
+		img2 = (ImageView)findViewById(R.id.MusicIcon2);
+		imgbtn1 = (ImageButton)findViewById(R.id.MusicIconButton1);
+		imgbtn2 = (ImageButton)findViewById(R.id.MusicIconButton2);
 		
-		Animation anim1 = AnimationUtils.loadAnimation(this, R.anim.img1_anim);
+		btn1 = (Button)findViewById(R.id.button1);
+		btn2 = (Button)findViewById(R.id.button2);
+		btn3 = (Button)findViewById(R.id.button3);
+		btn4 = (Button)findViewById(R.id.button4);
 		
-		//繰り返す回数を設定（１度でよい場合は設定しない）
-		anim1.setInterpolator(new CycleInterpolator(10));
+		btn1.setOnTouchListener(this);
+		btn2.setOnTouchListener(this);
+        btn3.setOnTouchListener(this);
+        btn4.setOnTouchListener(this);
 		
+		anim1 = AnimationUtils.loadAnimation(this, R.anim.img1_anim);
+		anim2 = AnimationUtils.loadAnimation(this, R.anim.imgbtn1_anim);
 		
-		ImageButton imgbtn1 = (ImageButton)findViewById(R.id.MusicIconButton1);
-		ImageButton imgbtn2 = (ImageButton)findViewById(R.id.MusicIconButton2);
+//		//繰り返す回数を設定（１度でよい場合は設定しない）
+//		anim1.setInterpolator(new CycleInterpolator(10));
+																	
+//		//繰り返す回数を設定（１度でよい場合は設定しない）
+//		anim2.setInterpolator(new CycleInterpolator(10));
 		
-		Animation anim2 = AnimationUtils.loadAnimation(this, R.anim.imgbtn1_anim);
-		
-		//繰り返す回数を設定（１度でよい場合は設定しない）
-		anim2.setInterpolator(new CycleInterpolator(10));
-		
-//		//アニメーションが無限に繰り返される
-//		anim1.setRepeatCount( Animation.INFINITE );
-		
-		//アニメーションをスタート
 		img1.startAnimation(anim1);
 		img2.startAnimation(anim1);
 		imgbtn1.startAnimation(anim2);
 		imgbtn2.startAnimation(anim2);
-
 	}
 
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		
+		//アニメーションをスタート
+		if(event.getAction() == MotionEvent.ACTION_DOWN) {
+			switch (v.getId()) {
+			case R.id.button1:
+				img1.startAnimation(anim1);
+				break;
+			case R.id.button2:
+				img2.startAnimation(anim1);
+				break;
+			case R.id.button3:
+				imgbtn1.startAnimation(anim2);
+				break;
+			case R.id.button4:
+				imgbtn2.startAnimation(anim2);
+				break;
+			}
+			return true;
+//		}else if(event.getAction() == MotionEvent.ACTION_UP) {
+//			switch (v.getId()) {
+//			case R.id.button1:
+//				img1.stopAnimation(anim1);
+//				break;
+//			case R.id.button2:
+//				img2.stopAnimation(anim1);
+//				break;
+//			case R.id.button3:
+//				imgbtn1.stopAnimation(anim2);
+//				break;
+//			case R.id.button4:
+//				imgbtn2.stopAnimation(anim2);
+//				break;
+//			}
+//		return true;
+		}
+		return false;
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
